b9753d3a0e0dc769a8cfa685bd9f79754e774082
Veronica <vveronikanika@gmail.com>
2022-11-11 13:39:18 +0200

    feat: create obj and save data to localStarage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+import throttle from 'lodash.throttle';
 
+const formElement = document.querySelector('form.feedback-form');
+const formData = {};
+
+console.log(formElement.elements);
+
+const handleInputForm = event => {
+  // const localObj = localStorage.setItem('formInput', event.target.elements);
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  console.log(formData);
+  // getInputFromLS(formData);
+};
+
+// function getInputFromLS(formData){
+// 	const savedObj = localStorage.
+// }
+
+formElement.addEventListener('input', handleInputForm);
